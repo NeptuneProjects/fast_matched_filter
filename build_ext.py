9d5f48ec96bff443db00a508c38a6c32a3c2e49f
William Jenkinsu <wjenkins@ucsd.edu>
2025-04-28 10:29:06 -0700

    t/s build

--- build_ext.py
+++ build_ext.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Custom `build_ext` module for building C extensions for FastMatchedFilter."""
 
+from pathlib import Path
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
 import subprocess
@@ -13,6 +14,9 @@ class FMFExtension(Extension):
 
 class FastMatchedFilterBuild(build_ext):
     def run(self) -> None:
+        lib_dir = Path(__file__).parent / "lib"
+        lib_dir.mkdir(parents=True, exist_ok=True)
+
         commands = ["python_cpu", "python_gpu"]
         for command in commands:
             ret = subprocess.call(["make", command])
